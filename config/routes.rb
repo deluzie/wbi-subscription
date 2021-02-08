d9e663f5d954d853f55cc513f7d12d2d02a82e56
Luzie Deubel <luzie@deubels.de>
2021-02-08 11:50:04 +0100

    added devise and git_admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :subscribers
   root to: 'subscribers#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
