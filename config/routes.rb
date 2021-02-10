d4128cdb0a4704b2924d2918535afaafa1188561
Luzie Deubel <luzie@deubels.de>
2021-02-10 12:29:22 +0100

    added route and page for contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  resources :subscribers
+  resources :subscribers, only: [:index, :create]
   root to: 'subscribers#index'
+  get "/kontakt", to: "application#kontakt"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
